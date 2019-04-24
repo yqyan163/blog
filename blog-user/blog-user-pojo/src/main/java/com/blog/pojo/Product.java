1981174267dbd8f888c627695ce20c41783546e5
13914 <13914@DESKTOP-U3EI512>
2019-04-24 23:19:37 +0800

    product commit

---
+++ blog-user/blog-user-pojo/src/main/java/com/blog/pojo/Product.java
@@ -0,0 +1,16 @@
+package com.blog.pojo;
+
+public class Product {
+
+	private String productName;
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	
+	
+}
